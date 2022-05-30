61a7cf1b49569a4abb8262fcdbbeab061b577be1
BangGao <greenbacyon@gmail.com>
2022-05-30 14:33:10 +0100

    Event Temp 1

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,39 +1,52 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using StarterAssets;
+using UnityEngine.Events;
+
 public class MenuController : MonoBehaviour
 {
-
+    public UnityAction _action;
+    public UnityEvent _myevent; 
+    
     public GameObject _uIElement_Field;
     private GameObject _objectGetByRay; 
     public string ObjectName;
+
+    
+    private PlayerInput _playerInput;
+    private StarterAssetsInputs _input;
     
     private Camera _cameraRay;
-    private bool isClosed;
-
+    private bool _isOpened;
+    
+    
     void Start()
     {
+        _action = new UnityAction(plantMenu);
+        _myevent = new UnityEvent();
+        _myevent.AddListener(_action);
+        
         _cameraRay = Camera.main;
+        _input = GetComponent<StarterAssetsInputs>();
+        _playerInput = GetComponent<PlayerInput>();
     }
-
-    void plantMenu()
-    {
-        if (Keyboard.current.eKey.isPressed)
-        {
-            isClosed = !isClosed;
-        }
-        _uIElement_Field.SetActive(isClosed);
-    }
-   
+    
     void Update()
     {
-        plantMenu();
+        _myevent.Invoke();
         CatchObjectByRay();
     }
-
+    
+    private void plantMenu()
+    {
+        _uIElement_Field.SetActive(_isOpened);
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            _isOpened = !_isOpened;
+            Debug.Log("Press Find");
+        }
+    }
+    
     private void CatchObjectByRay()
     {
         Ray ray = _cameraRay.ScreenPointToRay(Mouse.current.position.ReadValue());
